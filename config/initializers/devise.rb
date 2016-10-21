ce3c45c2aba5278497dc1de3b70223c3d1de83c8
sawadanoritaka <snp.inc.info@gmail.com>
2016-10-21 18:49:30 +0900

    aaaaaa

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,12 +2,13 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
 
-
-
-
+  if Rails.env.production?
+    config.omniauth :facebook, ENV["FACEBOOK_ID_PRODUCTION"], ENV["FACEBOOK_SECRET_PRODUCTION"], scope: 'email', display: 'popup', info_fields: 'name, email'
+    config.omniauth :twitter, ENV["TWITTER_ID_PRODUCTION"], ENV["TWITTER_SECRET_PRODUCTION"], scope: 'email', display: 'popup', info_fields: 'name, email'
+  else
     config.omniauth :facebook, ENV["FACEBOOK_ID_DEVELOPMENT"], ENV["FACEBOOK_SECRET_DEVELOPMENT"], scope: 'email', display: 'popup', info_fields: 'name, email'
     config.omniauth :twitter, ENV["TWITTER_ID_DEVELOPMENT"], ENV["TWITTER_SECRET_DEVELOPMENT"], scope: 'email', display: 'popup', info_fields: 'name, email'
-
+  end
 
 
   # The secret key used by Devise. Devise uses this key to generate
@@ -280,4 +281,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.secret_key = 'secret_key'
 end
